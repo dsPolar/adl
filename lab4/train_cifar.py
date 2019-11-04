2cfee0ed02452393c0178aa3d4d1ba83eff387ba
dsPolar <ds16797@bristol.ac.uk>
2019-11-04 13:22:54 +0000

    Lab4

--- lab4/train_cifar.py
+++ lab4/train_cifar.py
@@ -27,6 +27,10 @@ default_dataset_dir = Path.home() / ".cache" / "torch" / "datasets"
 parser.add_argument("--dataset-root", default=default_dataset_dir)
 parser.add_argument("--log-dir", default=Path("logs"), type=Path)
 parser.add_argument("--learning-rate", default=1e-2, type=float, help="Learning rate")
+parser.add_argument("--data-aug-hflip", action="store_true")
+parser.add_argument("--data-aug-brightness", default=0, type=float)
+parser.add_argument("--data-aug-rotation", default=0, type=float)
+parser.add_argument("--dropout", default=0, type=float)
 parser.add_argument(
     "--batch-size",
     default=128,
@@ -79,11 +83,29 @@ else:
 
 
 def main(args):
-    transform = transforms.ToTensor()
+
+    transform = transforms.Compose([
+    torchvision.transforms.ColorJitter(brightness=args.data_aug_brightness),
+    torchvision.transforms.RandomAffine(args.data_aug_rotation),
+    torchvision.transforms.ToTensor(),
+    ])
+
+    transform_flip = transforms.Compose([
+    torchvision.transforms.ColorJitter(brightness=args.data_aug_brightness),
+    torchvision.transforms.RandomAffine(args.data_aug_rotation),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    ])
     args.dataset_root.mkdir(parents=True, exist_ok=True)
-    train_dataset = torchvision.datasets.CIFAR10(
-        args.dataset_root, train=True, download=True, transform=transform
-    )
+    if(args.data_aug_hflip):
+        train_dataset = torchvision.datasets.CIFAR10(
+            args.dataset_root, train=True, download=True, transform=transform_flip
+        )
+    else:
+        train_dataset = torchvision.datasets.CIFAR10(
+            args.dataset_root, train=True, download=True, transform=transform
+        )
+
     test_dataset = torchvision.datasets.CIFAR10(
         args.dataset_root, train=False, download=False, transform=transform
     )
@@ -102,7 +124,7 @@ def main(args):
         pin_memory=True,
     )
 
-    model = CNN(height=32, width=32, channels=3, class_count=10)
+    model = CNN(height=32, width=32, channels=3, class_count=10, dropout=args.dropout)
 
     ## TASK 8: Redefine the criterion to be softmax cross entropy
     criterion = nn.CrossEntropyLoss()
@@ -131,10 +153,12 @@ def main(args):
 
 
 class CNN(nn.Module):
-    def __init__(self, height: int, width: int, channels: int, class_count: int):
+    def __init__(self, height: int, width: int, channels: int, class_count: int, dropout: float):
         super().__init__()
         self.input_shape = ImageShape(height=height, width=width, channels=channels)
         self.class_count = class_count
+
+        self.dropout = nn.Dropout(p=dropout)
 
         self.conv1 = nn.Conv2d(
             in_channels=self.input_shape.channels,
@@ -191,9 +215,9 @@ class CNN(nn.Module):
         ##         (batch_size, 4096)
         x = torch.flatten(x, start_dim=1)
         ## TASK 5-2: Pass x through the first fully connected layer
-        x = F.relu(self.fcnorm1(self.fc1(x)))
+        x = F.relu(self.fcnorm1(self.fc1(self.dropout(x))))
         ## TASK 6-2: Pass x through the last fully connected layer
-        x = self.fc2(x)
+        x = self.fc2(self.dropout(x))
         return x
 
     @staticmethod
@@ -333,6 +357,10 @@ class Trainer:
         accuracy = compute_accuracy(
             np.array(results["labels"]), np.array(results["preds"])
         )
+        perclass = compute_perclass_accuracy(
+            np.array(results["labels"]), np.array(results["preds"])
+        )
+
         average_loss = total_loss / len(self.val_loader)
 
         self.summary_writer.add_scalars(
@@ -340,12 +368,24 @@ class Trainer:
                 {"test": accuracy},
                 self.step
         )
+
         self.summary_writer.add_scalars(
                 "loss",
                 {"test": average_loss},
                 self.step
         )
+        print("airplane", perclass[0])
+        print("automobile", perclass[1])
+        print("bird", perclass[2])
+        print("cat", perclass[3])
+        print("deer", perclass[4])
+        print("dog", perclass[5])
+        print("frog", perclass[6])
+        print("horse", perclass[7])
+        print("ship", perclass[8])
+        print("truck", perclass[9])
         print(f"validation loss: {average_loss:.5f}, accuracy: {accuracy * 100:2.2f}")
+
 
 
 def compute_accuracy(
@@ -359,6 +399,12 @@ def compute_accuracy(
     assert len(labels) == len(preds)
     return float((labels == preds).sum()) / len(labels)
 
+def compute_perclass_accuracy(
+    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]):
+    accuracies = []
+    for i in range(0,10):
+        accuracies.append(float((((labels == i) & (preds == i)).sum()) / (labels == i).sum()))
+    return accuracies
 
 def get_summary_writer_log_dir(args: argparse.Namespace) -> str:
     """Get a unique directory that hasn't been logged to before for use with a TB
@@ -372,7 +418,17 @@ def get_summary_writer_log_dir(args: argparse.Namespace) -> str:
         from getting logged to the same TB log directory (which you can't easily
         untangle in TB).
     """
-    tb_log_dir_prefix = f'CNN_bs={args.batch_size}_lr={args.learning_rate}_momentum=0.9_run_'
+    tb_log_dir_prefix = (
+      f"CNN_bn_"
+      f"dropout={args.dropout}_"
+      f"bs={args.batch_size}_"
+      f"lr={args.learning_rate}_"
+      f"momentum=0.9_"
+      f"brightness={args.data_aug_brightness}_" +
+      ("hflip_" if args.data_aug_hflip else "") +
+      f"rotation={args.data_aug_rotation}_" +
+      f"run_"
+    )
     i = 0
     while i < 1000:
         tb_log_dir = args.log_dir / (tb_log_dir_prefix + str(i))
